fb6121b7843ba8dbbf3419ba622e617f41d7a7b3
Yogeshp0012 <yogeshp0012@gmail.com>
2024-06-19 18:39:00 +0530

    Fix host

--- src/app.js
+++ src/app.js
@@ -5,11 +5,13 @@ const { LoopsClient } = require("loops");
 const newsletter = require('./newsletters/newsletter');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const loops = new LoopsClient(process.env.LOOPS_KEY ?? "");
 
 app.use(express.json());
 newsletter(app, loops);
 
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+app.listen(port, "0.0.0.0", function () {
+    console.log(`Server is listening on port ${port}`)
+  });
